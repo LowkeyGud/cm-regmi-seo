eceb1476726d398f979fc1513d549757bfb628ff
lowkeygud <foreducationalonlytbh@gmail.com>
2026-05-04 19:15:10 +0545

    change

--- vite.config.ts
+++ vite.config.ts
@@ -5,25 +5,9 @@
 //     error logger plugins, and sandbox detection (port/host/strictPort).
 // You can pass additional config via defineConfig({ vite: { ... } }) if needed.
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
-import Sitemap from "vite-plugin-sitemap";
 
 export default defineConfig({
   vite: {
     base: "/", // Adjust this if your app is not hosted at the root of the domain
-    plugins: [
-      Sitemap({
-        hostname: "https://cmregmi.com.np",
-        dynamicRoutes: [
-          "/",
-          "/about",
-          "/docs",
-          "/contact",
-          "/ads-and-cookies",
-          "/privacy-policy",
-          "/terms-of-service",
-        ],
-        generateRobotsTxt: true,
-      }),
-    ],
   },
 });
